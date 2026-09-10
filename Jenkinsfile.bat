dfff232b80ecd6b4749b69dcceb6bcf5478bbced
shriram2007721 <swethashriram123@gmail.com>
2026-09-10 21:56:06 +0530

    Create Jenkinsfile.bat

---
+++ Jenkinsfile.bat
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build') {
+            steps {
+                bat 'mvn clean compile'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                bat 'mvn test'
+            }
+        }
+
+        stage('Package') {
+            steps {
+                bat 'mvn package -DskipTests'
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Build and tests completed successfully!'
+        }
+
+        failure {
+            echo 'Build or tests failed!'
+        }
+    }
+}
