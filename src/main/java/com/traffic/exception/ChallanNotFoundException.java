cfdf04304a186c6f8454cab2a727307a4a46854b
shriram2007721 <swethashriram123@gmail.com>
2026-09-10 22:07:54 +0530

    Create ChallanNotFoundException.java

---
+++ src/main/java/com/traffic/exception/ChallanNotFoundException.java
@@ -0,0 +1,8 @@
+package com.traffic.exception;
+
+public class ChallanNotFoundException extends RuntimeException {
+
+    public ChallanNotFoundException(String message) {
+        super(message);
+    }
+}
